47c570dce29163b4a760274fe0020e16586dc229
Jan Cimicky <ravanche@seznam.cz>
2019-03-06 18:19:06 +0100

    First HW - file types script.

---
+++ uloha01.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+input=$1
+if [ $input = '--typ' ]; then
+	input=$2
+	if [ -e $input ]; then
+		if [ -L $input ]; then
+			echo "symbolic link"
+			exit 0
+		elif [ -f $input ]; then
+			echo "regular file"
+			exit 0
+		elif [ -d $input ]; then
+			echo "directory"
+			exit 0
+		else
+			echo "unknown file type"
+			exit 0
+		fi
+	else
+		echo "File "$input" does not exist!"
+		exit 1
+	fi
+elif [ $input = '--help' ]; then
+	echo "uloha01.sh [--typ|--help] [path]"
+	echo "	--typ	[path] expected, returns type of file given by path"
+	echo "	--help	you already figured out"
+	exit 0
+else
+	echo "Unknown command!"
+	echo "input="$input
+	exit 1
+fi
