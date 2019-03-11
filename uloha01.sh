0964610dd414a4f4c6348c7996a9d6f9b30869ab
Jan Cimicky <ravanche@seznam.cz>
2019-03-11 09:10:50 +0100

    Upraveno tak, aby odpovídalo rozšířenému zadání s testy.

--- uloha01.sh
+++ uloha01.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
 input=$1
-if [ $input = '--typ' ]; then
+if [ "$input" = '--typ' ]; then
 	input=$2
-	if [ -e $input ]; then
-		if [ -L $input ]; then
+	if [ -e "$input" ]; then
+		if [ -L "$input" ]; then
 			echo "symbolic link"
 			exit 0
-		elif [ -f $input ]; then
+		elif [ -f "$input" ]; then
 			echo "regular file"
 			exit 0
-		elif [ -d $input ]; then
+		elif [ -d "$input" ]; then
 			echo "directory"
+			exit 0
+		elif [ -b "$input" ]; then
+			echo "block special"
+			exit 0
+		elif [ -c "$input" ]; then
+			echo "character special"
+			exit 0
+		elif [ -p "$input" ]; then
+			echo "named pipe"
 			exit 0
 		else
 			echo "unknown file type"
@@ -21,7 +30,7 @@ if [ $input = '--typ' ]; then
 		echo "File "$input" does not exist!"
 		exit 1
 	fi
-elif [ $input = '--help' ]; then
+elif [ "$input" = '--help' ]; then
 	echo "uloha01.sh [--typ|--help] [path]"
 	echo "	--typ	[path] expected, returns type of file given by path"
 	echo "	--help	you already figured out"
